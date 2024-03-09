ca52b60f7d6197092dc7073d4fd389caa81815f2
MitkoDG <retimid@abv.bg>
2024-03-09 16:30:07 +0200

    added auth routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from "./routes/auth.js"
 
 const app = express();
 
-const result = dotenv.config({ path: '../.env' });
+const result = dotenv.config({ path: "../.env" });
+
+if (result.error) {
+    console.error("Error loading .env file:", result.error);
+}
+
 const PORT = 8000 || process.env.PORT; //swap when ready
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`The server listening on PORT ${PORT}`)
