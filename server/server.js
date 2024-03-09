64ce8f4576d1280c37ce6a55e9eaf86531eb1540
MitkoDG <retimid@abv.bg>
2024-03-09 16:05:47 +0200

    fixing error with dotenv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("")
-});
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+const result = dotenv.config({ path: '../.env' });
+const PORT = 8000 || process.env.PORT; //swap when ready
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`The server listening on PORT ${PORT}`)
+})
