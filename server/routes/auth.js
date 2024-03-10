a63be909eb81f40fd072de6c8df83e98d140c239
MitkoDG <retimid@abv.bg>
2024-03-10 22:28:49 +0200

    adding controllers plus auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth..controller";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-    res.send("Login Route")
-});
+router.get("/api/auth/signup", signup);
+
+router.get("/api/auth/login", login);
+
+router.get("/api/auth/logout", logout);
 
 export default router;
