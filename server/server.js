f99cb5b10322ad41e4f189a17260ab608eab3342
MitkoDG <retimid@abv.bg>
2024-03-11 23:55:00 +0200

    create User Schema

--- server/server.js
+++ server/server.js
@@ -5,14 +5,16 @@ import authRoutes from "./routes/auth.js"
 import connectToMongoDB from './db/connectToMondoDB.js';
 
 const app = express();
-
-const result = dotenv.config({ path: ".env" }); // need to be fixed
-
-if (result.error) {
-    console.error("Error loading .env file:", result.error);
-}
-
 const PORT = 8000 || process.env.PORT; //swap when ready
+
+dotenv.config();
+
+// const result = dotenv.config({ path: ".env" }); // need to be fixed
+
+// if (result.error) {
+//     console.error("Error loading .env file:", result.error);
+// }
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
