61658f20b61affc5fdca8579c214b1fd67647215
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2024-03-21 15:44:45 +0200

    added login function to auth,controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -52,9 +52,28 @@ export const signup = async (req, res) => {
   console.log("signupUser");
 };
 
-export const login = (req, res) => {
-  res.send("loginUser");
-  console.log("loginUser");
+export const login = async (req, res) => {
+  try {
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json( {error: "Wrong username or password"} );
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+    
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
