9b8b03693374191b5c84175c26b92c5961a970ed
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2024-03-21 15:06:21 +0200

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,57 +1,61 @@
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-    try {
-        const { fullName, username, password, confirmPassword, gender } = req.body;
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
 
-        if (password !== confirmPassword) {
-            return res.status(400).json({ error: "Passwords don`t match" })
-        }
-
-        const user = await User.findOne({ username })
-
-        if (user) {
-            return res.status(400).json({error: "User already exists"})
-        }
-
-        // HASH password here
-        const salt = await bcrypt.getSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        // avatar: https://avatar.iran.liara.run/public/boy
-
-        const boyProfilPic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-        const girlProfilPic = `https://avatar.iran.liara.run/public/girl?username=${username}`
-
-        const newUser = new User({
-            fullName,
-            username,
-            password: hashedPassword,
-            gender,
-            profilePic: gender === "male" ? boyProfilPic : girlProfilPic
-        })
-
-        await newUser.save();
-
-        res.status(201).json({
-            _id: newUser._id,
-            fullname: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
-    } catch (error) {
-        console.log("Error in signup controller", error.message);
-        res.status(500).json({error: "Internal Server Error"})
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don`t match" });
     }
-    console.log("signupUser");
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    // HASH password here
+    const salt = await bcrypt.getSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // avatar: https://avatar.iran.liara.run/public/boy
+
+    const boyProfilPic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilPic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilPic : girlProfilPic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+  console.log("signupUser");
 };
 
 export const login = (req, res) => {
-    res.send("loginUser")
-    console.log("loginUser");
+  res.send("loginUser");
+  console.log("loginUser");
 };
 
 export const logout = (req, res) => {
-    res.send("logoutUser")
-    console.log("logoutUser");
+  res.send("logoutUser");
+  console.log("logoutUser");
 };
